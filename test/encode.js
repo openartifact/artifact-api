6b17954358bc40bba1965f47966ab0aac375df7f
Jason Coene <jcoene@gmail.com>
2018-11-19 15:41:43 -0600

    allow encoding multiple of the same hero

--- test/encode.js
+++ test/encode.js
@@ -1,6 +1,7 @@
 const { assert } = require('chai');
 const { encode } = require('../lib');
 const greenBlackDeck = require('./green-black');
+const multipleSameHero = require('./multiple-same-hero');
 
 describe('Encoding Artifact Deck Code', () => {
   it('should fail when encoding non-object', () => {
@@ -19,5 +20,9 @@ describe('Encoding Artifact Deck Code', () => {
 
   it('should succeed', () => {
     assert.equal(encode(greenBlackDeck), 'ADCJWkTZX05uwGDCRV4XQGy3QGLmqUBg4GQJgGLGgO7AaABR3JlZW4vQmxhY2sgRXhhbXBsZQ__');
+  });
+
+  it('should successfully encode multiple of the same hero', () => {
+    assert.equal(encode(multipleSameHero), 'ADCJZ8ZZLkCAIAAAE11bHRpcGxlIG9mIHRoZSBzYW1lIGhlcm8_');
   });
 });
---
+++ test/multiple-same-hero.js
@@ -0,0 +1,26 @@
+module.exports = {
+  name: "Multiple of the same hero",
+  heroes: [
+    {
+      id: 10020, // Axe
+      turn: 2
+    },
+    {
+      id: 10020, // Axe
+      turn: 1
+    },
+    {
+      id: 10020, // Axe
+      turn: 3
+    },
+    {
+      id: 10020, // Axe
+      turn: 1
+    },
+    {
+      id: 10020, // Axe
+      turn: 1
+    }
+  ],
+  cards: []
+};
